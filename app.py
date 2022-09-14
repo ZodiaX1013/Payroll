97438f02450a33652ce557e34155a08d22316bc9
ZodiaX1013 <jaydeepgamit36@gmail.com>
2022-09-14 15:24:10 +0530

    commit

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.secret_key = "secret key"
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 # wkhtmltopdf = Wkhtmltopdf(app)
-pdfkit.configuration(wkhtmltopdf = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], stdout=subprocess.PIPE).communicate()[0].strip())
+# pdfkit.configuration(wkhtmltopdf = subprocess.Popen(['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf')], stdout=subprocess.PIPE).communicate()[0].strip())
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif','pdf'])
 # config = pdfkit.configuration(wkhtmltopdf='./bin/wkhtmltopdf')
 
