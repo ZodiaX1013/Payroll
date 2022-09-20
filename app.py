505f8567e8d019c0ef40c46abae5448a8f5eb081
ZodiaX1013 <jaydeepgamit36@gmail.com>
2022-09-20 10:57:52 +0530

    commit

--- app.py
+++ app.py
@@ -1586,7 +1586,7 @@ def paysheet():
 
             # query1 = "SELECT * FROM paysheet"
             # query1 = "SELECT EmployeeName, BasicSalary, Arrears, Overseas, TravelAllow, OtherAllow, Gross, PAYE, CSG, NSF, Medical, SLevy, Net FROM paysheet"
-            query1 = "SELECT BasicSalary, Overtime, LeaveRef, EOY, TravelAllow, OtherAllow, CurrentGross, Absences, CurrentPAYE, NPS, NSFEmpee, Medical, SLevy, Lateness, Deduction, NetPay FROM salary"
+            query1 = "SELECT EmployeeName, BasicSalary, Arrears, Overtime, LeaveRef, EOY, TravelAllow, OtherAllow, CurrentGross, Absences, CurrentPAYE, NPS, NSFEmpee, Medical, SLevy, Lateness, Deduction, NetPay FROM salary"
             cursor.execute(query1)
             data = cursor.fetchall()
             print(data)
