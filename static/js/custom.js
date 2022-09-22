5f13ecba2408986d78542c359a6b136a5c7a5730
ZodiaX1013 <jaydeepgamit36@gmail.com>
2022-09-22 11:26:11 +0530

    commit

--- static/js/custom.js
+++ static/js/custom.js
@@ -606,41 +606,41 @@ function Export2Word(element, filename = 'paysheet'){
   * Tested with Word 2010, 2013 and FireFox, Chrome, Opera, IE10-11
   * Fails in legacy browsers (IE<10) that lack window.Blob object
   */
- window.export.onclick = function() {
+//  window.export.onclick = function() {
  
-  if (!window.Blob) {
-     alert('Your legacy browser does not support this action.');
-     return;
-  }
+//   if (!window.Blob) {
+//      alert('Your legacy browser does not support this action.');
+//      return;
+//   }
 
-  var html, link, blob, url, css;
+//   var html, link, blob, url, css;
   
-  // EU A4 use: size: 841.95pt 595.35pt;
-  // US Letter use: size:11.0in 8.5in;
+//   // EU A4 use: size: 841.95pt 595.35pt;
+//   // US Letter use: size:11.0in 8.5in;
   
-  css = (
-    '<style>' +
-    '@page WordSection1{size: 1191pt 842pt ;mso-page-orientation: Landscape;}' +
-    'div.WordSection1 {page: WordSection1;}' +
-    'table{border-collapse:collapse;}td{border:1px gray solid;width:5em;padding:2px;}'+
-    '</style>'
-  );
+//   css = (
+//     '<style>' +
+//     '@page WordSection1{size: 1191pt 842pt ;mso-page-orientation: Landscape;}' +
+//     'div.WordSection1 {page: WordSection1;}' +
+//     'table{border-collapse:collapse;}td{border:1px gray solid;width:5em;padding:2px;}'+
+//     '</style>'
+//   );
   
-  html = window.docx.innerHTML;
-  blob = new Blob(['\ufeff', css + html], {
-    type: 'application/msword'
-  });
-  url = URL.createObjectURL(blob);
-  link = document.createElement('A');
-  link.href = url;
-  // Set default file name. 
-  // Word will append file extension - do not add an extension here.
-  link.download = 'Document';   
-  document.body.appendChild(link);
-  if (navigator.msSaveOrOpenBlob ) navigator.msSaveOrOpenBlob( blob, 'Document.doc'); // IE10-11
-      else link.click();  // other browsers
-  document.body.removeChild(link);
-};
+//   html = window.docx.innerHTML;
+//   blob = new Blob(['\ufeff', css + html], {
+//     type: 'application/msword'
+//   });
+//   url = URL.createObjectURL(blob);
+//   link = document.createElement('A');
+//   link.href = url;
+//   // Set default file name. 
+//   // Word will append file extension - do not add an extension here.
+//   link.download = 'Document';   
+//   document.body.appendChild(link);
+//   if (navigator.msSaveOrOpenBlob ) navigator.msSaveOrOpenBlob( blob, 'Document.doc'); // IE10-11
+//       else link.click();  // other browsers
+//   document.body.removeChild(link);
+// };
 
 
 // Image Name Shown
